0c588c6f9e4459d49e067f050ddd25e7a4d576c2
Mirim Ju <wnalfla@gmail.com>
2019-05-22 11:57:17 +0900

    #1 폼디자인 작성 - 콤보 박스, 텍스트 박스, 라벨, 버튼을 추가하여 폼 디자인 작업 완료

--- CB_Choice/Form1.Designer.cs
+++ CB_Choice/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cdList = new System.Windows.Forms.ComboBox();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cdList
+            // 
+            this.cdList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cdList.FormattingEnabled = true;
+            this.cdList.Location = new System.Drawing.Point(12, 12);
+            this.cdList.Name = "cdList";
+            this.cdList.Size = new System.Drawing.Size(217, 20);
+            this.cdList.TabIndex = 0;
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(13, 39);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(216, 21);
+            this.txtList.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(10, 63);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(69, 12);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "선택 결과 : ";
+            this.lblResult.Click += new System.EventHandler(this.LblResult_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(235, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 193);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(326, 217);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.cdList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "메뉴 선택 추가";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cdList;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
